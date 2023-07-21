1f1f46d86697b7f60e41cd0b78516cd0b917cae1
baiango <105705580+baiango@users.noreply.github.com>
2023-07-21 20:14:36 +0800

    New function for text. str_to_texture()

--- Bevy GUI.c
+++ Bevy GUI.c
@@ -6,31 +6,20 @@
 #include "types.h"
 
 
-i32 main()
+int main()
 {	printf("Hi!\n");
 	TTF_Init();
 	SDL_Window *window;
 	SDL_Renderer *renderer;
 {	Vec2i window_size = { 800, 600 };
+	// Memory leak. Don't call SDL_CreateWindowAndRenderer() more than 1 time.
 	SDL_CreateWindowAndRenderer(window_size.x, window_size.y, SDL_WINDOW_OPENGL, &window, &renderer); }
 
 
-	SDL_Texture* text_texture;
-	SDL_Rect text_scale;
-{	SDL_Color text_color = { 0xff, 0xff, 0xff };
-	TTF_Font *font = TTF_OpenFont("pixel-clear-condensed.ttf", 24);
-	if (font == NULL) { printf("Font not found!\n"); goto error; }
+	TxtTexture text;
+{	SDL_Color col = { 0xff, 0xff, 0xff };
+	text = str_to_texture(renderer, "WWW! my grass!", 24, col, NO_PATH); }
 
-	SDL_Surface *text_surface = TTF_RenderText_Solid(font, "WWW! my grass!", text_color);
-	TTF_CloseFont(font);
-
-	text_texture = SDL_CreateTextureFromSurface(renderer, text_surface);
-	SDL_FreeSurface(text_surface);
-
-	Vec2i txtofst = { 10, 10 };
-	i32 txtw, txth;
-	SDL_QueryTexture(text_texture, NULL, NULL, &txtw, &txth);
-	text_scale = (SDL_Rect){ txtofst.x, txtofst.y, txtw, txth }; }
 
 	SDL_Event event;
 	while (1)
@@ -38,18 +27,18 @@ i32 main()
 		SDL_PollEvent(&event);
 		switch (event.type)
 		{case SDL_QUIT: goto quit; }
-
 		// Render: Clear
 		SDL_SetRenderDrawColor(renderer, 0x20, 0x20, 0x20, 0xff);
 		SDL_RenderClear(renderer);
 		// Render
-		SDL_RenderCopy(renderer, text_texture, NULL, &text_scale);
+		SDL_RenderCopy(renderer, text.text_texture, NULL, &text.text_stretch);
 		SDL_RenderPresent(renderer); }
 
-	// Free
-	SDL_DestroyTexture(text_texture);
-
-error:
-	return -1;
 quit:
+	// Free
+	SDL_DestroyTexture(text.text_texture);
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	TTF_Quit();
+	SDL_Quit();
 	printf("Quitting!\n"); }
--- types.h
+++ types.h
@@ -1,4 +1,7 @@
 #pragma once
+#include <stdlib.h>
+
+
 typedef unsigned char u8; typedef unsigned short u16; typedef unsigned u32; typedef unsigned long long u64;
 typedef char i8; typedef short i16; typedef int i32; typedef long long i64;
 typedef float f32; typedef double f64; typedef long double f80;
@@ -13,3 +16,36 @@ typedef struct {
 	i32 y;
 } Vec2i;
 
+typedef struct {
+	SDL_Color text_color;
+	SDL_Texture *text_texture;
+	SDL_Rect text_stretch;
+} TxtTexture;
+
+
+const void *NO_PATH = NULL;
+/* Example:
+   TxtTexture text = str_to_texture(renderer, "WWW! my grass!", 24, (SDL_Color){ 0xff, 0xff, 0xff }, NO_PATH);
+   SDL_DestroyTexture(text.text_texture);
+   It will cause memory leak if you don't destroy the texture. */
+TxtTexture str_to_texture(SDL_Renderer *renderer, char str[], int font_size, SDL_Color text_color, char font_path[])
+{	TxtTexture ret;
+{	TTF_Font *font;
+	if (font_path == NULL)
+	{	font = TTF_OpenFont("pixel-clear-condensed.ttf", font_size);
+		if (font == NULL) { printf("Font not found! Panic!\n"); exit(-1); }}
+	else
+	{	font = TTF_OpenFont(font_path, font_size);
+		if (font == NULL) printf("Font not found! Ignoring!\n"); }
+
+	SDL_Surface *text_surface = TTF_RenderText_Solid(font, str, text_color);
+	TTF_CloseFont(font);
+	ret.text_texture = SDL_CreateTextureFromSurface(renderer, text_surface);
+	SDL_FreeSurface(text_surface);
+
+	Vec2i txtofst = { 10, 10 };
+	i32 txtw, txth;
+	SDL_QueryTexture(ret.text_texture, NULL, NULL, &txtw, &txth);
+	ret.text_stretch = (SDL_Rect){ txtofst.x, txtofst.y, txtw, txth }; }
+	
+	return ret; }
