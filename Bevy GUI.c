1179872a4d940511bcb310f8a940af4fa37049fd
baiango <105705580+baiango@users.noreply.github.com>
2023-07-21 17:05:48 +0800

    Drew my own font in SDL2!

--- Bevy GUI.c
+++ Bevy GUI.c
@@ -1,18 +1,55 @@
 #include <stdio.h>
 #define SDL_MAIN_HANDLED
 #include <sdl2/SDL.h>
-//#undef main
+#include <sdl2/SDL_image.h>
+#include <sdl2/SDL_ttf.h>
+#include "types.h"
 
-void func_to_show_mem_leak()
-{	int* ptr = malloc(sizeof(int)); } // Found by cppcheck and intel inspector
 
-
-int main()
+i32 main()
 {	printf("Hi!\n");
-	func_to_show_mem_leak();
-	int *ptr = malloc(sizeof(int));
-	int **dangling = &ptr;
-	free(*dangling);
-	free(ptr);  // Found by intellisense and intel inspector
-	free(ptr); // Found by cppcheck and intel inspector
-}
+	TTF_Init();
+	SDL_Window *window;
+	SDL_Renderer *renderer;
+{	Vec2i window_size = { 800, 600 };
+	SDL_CreateWindowAndRenderer(window_size.x, window_size.y, SDL_WINDOW_OPENGL, &window, &renderer); }
+
+
+	SDL_Texture* text_texture;
+	SDL_Rect text_scale;
+{	SDL_Color text_color = { 0xff, 0xff, 0xff };
+	TTF_Font *font = TTF_OpenFont("pixel-clear-condensed.ttf", 24);
+	if (font == NULL) { printf("Font not found!\n"); goto error; }
+
+	SDL_Surface *text_surface = TTF_RenderText_Solid(font, "WWW! my grass!", text_color);
+	TTF_CloseFont(font);
+
+	text_texture = SDL_CreateTextureFromSurface(renderer, text_surface);
+	SDL_FreeSurface(text_surface);
+
+	Vec2i txtofst = { 10, 10 };
+	i32 txtw, txth;
+	SDL_QueryTexture(text_texture, NULL, NULL, &txtw, &txth);
+	text_scale = (SDL_Rect){ txtofst.x, txtofst.y, txtw, txth }; }
+
+	SDL_Event event;
+	while (1)
+	{	// Poll
+		SDL_PollEvent(&event);
+		switch (event.type)
+		{case SDL_QUIT: goto quit; }
+
+		// Render: Clear
+		SDL_SetRenderDrawColor(renderer, 0x20, 0x20, 0x20, 0xff);
+		SDL_RenderClear(renderer);
+		// Render
+		SDL_RenderCopy(renderer, text_texture, NULL, &text_scale);
+		SDL_RenderPresent(renderer); }
+
+	// Free
+	SDL_DestroyTexture(text_texture);
+
+error:
+	return -1;
+quit:
+	printf("Quitting!\n"); }
---
+++ types.h
@@ -0,0 +1,15 @@
+#pragma once
+typedef unsigned char u8; typedef unsigned short u16; typedef unsigned u32; typedef unsigned long long u64;
+typedef char i8; typedef short i16; typedef int i32; typedef long long i64;
+typedef float f32; typedef double f64; typedef long double f80;
+
+typedef struct {
+	f32 x;
+	f32 y;
+} Vec2f;
+
+typedef struct {
+	i32 x;
+	i32 y;
+} Vec2i;
+
