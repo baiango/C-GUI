d2a411ded49b58f0c7f58d6a285cbdba591d7180
baiango <105705580+baiango@users.noreply.github.com>
2023-07-22 09:34:10 +0800

    Front-end C! Reformatted the code

--- Bevy GUI.c
+++ Bevy GUI.c
@@ -7,38 +7,42 @@
 
 
 int main()
-{	printf("Hi!\n");
-	TTF_Init();
-	SDL_Window *window;
-	SDL_Renderer *renderer;
-{	Vec2i window_size = { 800, 600 };
-	// Memory leak. Don't call SDL_CreateWindowAndRenderer() more than 1 time.
-	SDL_CreateWindowAndRenderer(window_size.x, window_size.y, SDL_WINDOW_OPENGL, &window, &renderer); }
+{	printf("Hi!\n")
+;	TTF_Init()
+;	SDL_Window *window
+;	SDL_Renderer *renderer
+;{	Vec2i window_size = { 800, 600 }
+;	// Memory leak. Don't call SDL_CreateWindowAndRenderer() more than 1 time.
+	SDL_CreateWindowAndRenderer(window_size.x, window_size.y, SDL_WINDOW_OPENGL, &window, &renderer)
+;}
 
 
-	TxtTexture text;
-{	SDL_Color col = { 0xff, 0xff, 0xff };
-	text = str_to_texture(renderer, "WWW! my grass!", 24, col, NO_PATH); }
+	TxtTexture text
+;{	SDL_Color col = { 0xff, 0xff, 0xff }
+;	text = str_to_texture(renderer, "WWW! my grass!", 24, col, NO_PATH)
+;}
 
-
-	SDL_Event event;
-	while (1)
+	SDL_Event event
+;	while (1)
 	{	// Poll
-		SDL_PollEvent(&event);
-		switch (event.type)
-		{case SDL_QUIT: goto quit; }
+		SDL_PollEvent(&event)
+	;	switch (event.type)
+		{case SDL_QUIT: goto quit
+		;}
 		// Render: Clear
-		SDL_SetRenderDrawColor(renderer, 0x20, 0x20, 0x20, 0xff);
-		SDL_RenderClear(renderer);
-		// Render
-		SDL_RenderCopy(renderer, text.text_texture, NULL, &text.text_stretch);
-		SDL_RenderPresent(renderer); }
+		SDL_SetRenderDrawColor(renderer, 0x20, 0x20, 0x20, 0xff)
+	;	SDL_RenderClear(renderer)
+	;	// Render
+		SDL_RenderCopy(renderer, text.text_texture, NULL, &text.text_stretch)
+	;	SDL_RenderPresent(renderer)
+	;}
 
 quit:
 	// Free
-	SDL_DestroyTexture(text.text_texture);
-	SDL_DestroyRenderer(renderer);
-	SDL_DestroyWindow(window);
-	TTF_Quit();
-	SDL_Quit();
-	printf("Quitting!\n"); }
+	SDL_DestroyTexture(text.text_texture)
+;	SDL_DestroyRenderer(renderer)
+;	SDL_DestroyWindow(window)
+;	TTF_Quit()
+;	SDL_Quit()
+;	printf("Quitting!\n")
+; }
--- types.h
+++ types.h
@@ -6,16 +6,9 @@ typedef unsigned char u8; typedef unsigned short u16; typedef unsigned u32; type
 typedef char i8; typedef short i16; typedef int i32; typedef long long i64;
 typedef float f32; typedef double f64; typedef long double f80;
 
-typedef struct {
-	f32 x;
-	f32 y;
-} Vec2f;
 
-typedef struct {
-	i32 x;
-	i32 y;
-} Vec2i;
-
+typedef struct { f32 x, y; } Vec2f;
+typedef struct { i32 x, y; } Vec2i;
 typedef struct {
 	SDL_Color text_color;
 	SDL_Texture *text_texture;
@@ -29,23 +22,26 @@ const void *NO_PATH = NULL;
    SDL_DestroyTexture(text.text_texture);
    It will cause memory leak if you don't destroy the texture. */
 TxtTexture str_to_texture(SDL_Renderer *renderer, char str[], int font_size, SDL_Color text_color, char font_path[])
-{	TxtTexture ret;
-{	TTF_Font *font;
-	if (font_path == NULL)
-	{	font = TTF_OpenFont("pixel-clear-condensed.ttf", font_size);
-		if (font == NULL) { printf("Font not found! Panic!\n"); exit(-1); }}
+{	TxtTexture ret
+;{	TTF_Font *font
+;	if (font_path == NULL)
+	{	font = TTF_OpenFont("pixel-clear-condensed.ttf", font_size)
+	;	if (font == NULL) { printf("Font not found! Panic!\n"); exit(-1); }
+	}
 	else
-	{	font = TTF_OpenFont(font_path, font_size);
-		if (font == NULL) printf("Font not found! Ignoring!\n"); }
+	{	font = TTF_OpenFont(font_path, font_size)
+	;	if (font == NULL) printf("Font not found! Ignoring!\n");
+	}
 
-	SDL_Surface *text_surface = TTF_RenderText_Solid(font, str, text_color);
-	TTF_CloseFont(font);
-	ret.text_texture = SDL_CreateTextureFromSurface(renderer, text_surface);
-	SDL_FreeSurface(text_surface);
-
-	Vec2i txtofst = { 10, 10 };
-	i32 txtw, txth;
-	SDL_QueryTexture(ret.text_texture, NULL, NULL, &txtw, &txth);
-	ret.text_stretch = (SDL_Rect){ txtofst.x, txtofst.y, txtw, txth }; }
-	
-	return ret; }
+	SDL_Surface *text_surface = TTF_RenderText_Solid(font, str, text_color)
+;	TTF_CloseFont(font)
+;	ret.text_texture = SDL_CreateTextureFromSurface(renderer, text_surface)
+;	SDL_FreeSurface(text_surface)
+;
+	Vec2i txtofst = { 10, 10 }
+;	i32 txtw, txth
+;	SDL_QueryTexture(ret.text_texture, NULL, NULL, &txtw, &txth)
+;	ret.text_stretch = (SDL_Rect){ txtofst.x, txtofst.y, txtw, txth }
+;}
+	return ret
+; }
