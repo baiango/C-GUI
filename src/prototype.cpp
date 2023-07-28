924daaec9271876949db1df6fc8c092d0f12c28c
baiango <105705580+baiango@users.noreply.github.com>
2023-07-28 13:24:37 +0800

    Validation layers 50% | C++ prototype
    i decided to covert the C++ code into DLL instead of trying to write it in Odin. then i will use Odin to control the C++ code. because Khronos might decide to break the API in the next day. and they use macros on debugCallback(), Odin doesn't support it. and the Vulkan bindings is a pain to maintain due to its size.

--- src/prototype.cpp
+++ src/prototype.cpp
@@ -7,20 +7,76 @@
 #include <glm/mat4x4.hpp>
 
 #include <iostream>
+#include <vector>
+using std::vector;
 
-int main()
-{	using std::cout;
+
+static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
+	VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+	VkDebugUtilsMessageTypeFlagsEXT messageType,
+	const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
+	void* pUserData
+) {
+
+	std::cerr << "validation layer: " << pCallbackData->pMessage << std::endl;
+
+	if (messageSeverity >= VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT) {
+		// Message is important enough to show
+	}
+
+	return VK_FALSE;
+}
+
+int main() {
+	using std::cout;
 	std::cout << "Hello World!\n";
 	glfwInit();
 	// GLFW_NO_API means no OpenGL api. That leaves Vulkan api to you only.
 	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
 	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
+	uint32_t WIDTH = 800, HEIGHT = 600;
 	GLFWwindow *window = glfwCreateWindow(800, 600, "Hello Vulkan!", nullptr, nullptr);
 
-	// Instance
+
+
+	// 2a: Validation layers
+	vector<const char*> validation_layers = { "VK_LAYER_KHRONOS_validation" };
+	bool is_validation_layer_supported = false;
+#ifdef NDEBUG
+	bool is_validation_layers_on = false;
+#else
+	bool is_validation_layers_on = true;
+#endif
+	// checkValidationLayerSupport()
+{
+	uint32_t layer_cout;
+	vkEnumerateInstanceLayerProperties(&layer_cout, nullptr);
+
+	vector<VkLayerProperties> available_layers(layer_cout);
+	vkEnumerateInstanceLayerProperties(&layer_cout, available_layers.data());
+
+	for (const char* layer_name : validation_layers) {
+		for (const auto& layer_properties : available_layers) {
+			if (strcmp(layer_name, layer_properties.layerName) == 0) {
+				is_validation_layer_supported = true;
+				goto checked_validation_layer_support;
+			}
+		}
+	}
+}
+checked_validation_layer_support:
+	if (is_validation_layers_on && is_validation_layer_supported) {
+		cout << "Validation layers is available!\n";
+	} else {
+		cout << "Validation layers requested, but not available!\n";
+	}
+
+
+
+	// 1: Instance
 	VkInstance instance;
-	VkApplicationInfo app_info;
+	VkApplicationInfo app_info{};
 	app_info.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
 	app_info.pApplicationName = "Hello Triangle";
 	app_info.applicationVersion = VK_MAKE_API_VERSION(1, 0, 0, 0);
@@ -39,22 +95,51 @@ int main()
 
 	create_info.enabledLayerCount = 0;
 
+
+
+	// 2b: Validation layers
+	if (is_validation_layers_on) {
+		create_info.enabledLayerCount = (uint32_t)(validation_layers.size());
+		create_info.ppEnabledLayerNames = validation_layers.data();
+	} else {
+		create_info.enabledLayerCount = 0;
+	}
+
+	// getRequiredExtensions()
+	vector<const char*> extensions;
+{
+	uint32_t glfw_ext_cout = 0;
+	const char** glfw_exts;
+	glfw_exts = glfwGetRequiredInstanceExtensions(&glfw_ext_cout);
+
+	vector<const char*> extensions(glfw_exts, glfw_exts + glfw_ext_cout);
+
+	if (is_validation_layers_on) {
+		extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+	}
+}
+
+	create_info.enabledExtensionCount = (uint32_t)(extensions.size());
+	create_info.ppEnabledExtensionNames = extensions.data();
+	// 50%
+
+
+	// Window
 	VkResult result = vkCreateInstance(&create_info, nullptr, &instance);
-	if (VK_SUCCESS != result) { cout << "Failed to create instance!"; }
-	//
+	if (VK_SUCCESS != result) { cout << "Failed to create instance! " << result; }
 
 	uint32_t extCout = 0;
 	vkEnumerateInstanceExtensionProperties(nullptr, &extCout, nullptr);
 	using std::cout;
-	cout << extCout << " extensions supported\n";
+	cout << extCout << " extensions supported.\n";
 
 	glm::mat4 matrix;
 	glm::vec4 vec;
 	auto test = matrix * vec;
 
-	while(!glfwWindowShouldClose(window)) {
-		glfwPollEvents();
-	}
+	//while (!glfwWindowShouldClose(window)) {
+	//	glfwPollEvents();
+	//}
 
 	vkDestroyInstance(instance, nullptr);
 	glfwDestroyWindow(window);
