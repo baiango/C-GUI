615a0707518402f70bed7263eea4dba8c83ac60d
baiango <105705580+baiango@users.noreply.github.com>
2023-07-30 21:50:00 +0800

    Switching to OpenGL 3.3
    I am very overwhelmed by the verboseness and difficulty of Vulkan. So I give up and trying to learn OpenGL instead.

--- src/prototype.cpp
+++ src/prototype.cpp
@@ -17,7 +17,7 @@ static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
 	const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
 	void* pUserData
 ) {
-	if (messageSeverity >= VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT) {
+	if (VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT < messageSeverity) {
 		// Message is important enough to show
 		std::cerr << "Validation layer: " << pCallbackData->pMessage << "\n";
 	}
@@ -34,8 +34,7 @@ int main() {
 	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
 	uint32_t WIDTH = 800, HEIGHT = 600;
-	GLFWwindow *window = glfwCreateWindow(800, 600, "Hello Vulkan!", nullptr, nullptr);
-
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Hello Vulkan!", nullptr, nullptr);
 
 
 	// 2a: Validation layers
@@ -47,29 +46,29 @@ int main() {
 	bool is_validation_layers_on = true;
 #endif
 	// checkValidationLayerSupport()
-{
-	uint32_t layer_cout;
-	vkEnumerateInstanceLayerProperties(&layer_cout, nullptr);
+	{
+		uint32_t layer_cout;
+		vkEnumerateInstanceLayerProperties(&layer_cout, nullptr);
 
-	vector<VkLayerProperties> available_layers(layer_cout);
-	vkEnumerateInstanceLayerProperties(&layer_cout, available_layers.data());
+		vector<VkLayerProperties> available_layers(layer_cout);
+		vkEnumerateInstanceLayerProperties(&layer_cout, available_layers.data());
 
-	for (const char* layer_name : validation_layers) {
-		for (const auto& layer_properties : available_layers) {
-			if (strcmp(layer_name, layer_properties.layerName) == 0) {
-				is_validation_layer_supported = true;
-				goto checked_validation_layer_support;
+		for (const char* layer_name : validation_layers) {
+			for (const auto& layer_properties : available_layers) {
+				if (strcmp(layer_name, layer_properties.layerName) == 0) {
+					is_validation_layer_supported = true;
+					goto checked_validation_layer_support;
+				}
 			}
 		}
 	}
-}
 checked_validation_layer_support:
 	if (is_validation_layers_on && is_validation_layer_supported) {
 		cout << "Validation layers is available!\n";
-	} else {
+	}
+	else {
 		cout << "Validation layers requested, but not available!\n";
 	}
-
 
 
 	// 1: Instance
@@ -94,14 +93,12 @@ checked_validation_layer_support:
 	create_info.enabledLayerCount = 0;
 
 
-
 	// 2b: Validation layers
 	VkDebugUtilsMessengerCreateInfoEXT debug_create_info{};
 	if (is_validation_layers_on) {
 		create_info.enabledLayerCount = (uint32_t)(validation_layers.size());
 		create_info.ppEnabledLayerNames = validation_layers.data();
 
-		debug_create_info = {};
 		debug_create_info.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
 		debug_create_info.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
 		debug_create_info.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
@@ -127,12 +124,67 @@ checked_validation_layer_support:
 
 	create_info.enabledExtensionCount = (uint32_t)(extensions.size());
 	create_info.ppEnabledExtensionNames = extensions.data();
-	// 50%
 
+	// setupDebugMessenger()
+	VkDebugUtilsMessengerEXT debug_messenger;
+	if (!is_validation_layers_on) { goto skip_debug_messenger; }
 
+	debug_create_info.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+	debug_create_info.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+	debug_create_info.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+	debug_create_info.pfnUserCallback = debugCallback;
+
+	//VkDebugUtilsMessengerCreateInfoEXT messenger_create_info;
+	//auto func = (PFN_vkCreateDebugUtilsMessengerEXT)vkGetInstanceProcAddr(instance, "vkCreateDebugUtilsMessengerEXT");
+	//if (nullptr == func) {
+	//	throw std::runtime_error("failed to set up debug messenger!");
+	//} else {
+	//	func(instance, &messenger_create_info, nullptr, &debug_messenger);
+	//}
+
+skip_debug_messenger:
 	// Window
 	VkResult result = vkCreateInstance(&create_info, nullptr, &instance);
 	if (VK_SUCCESS != result) { cout << "Failed to create instance! " << result; }
+
+	// 3a: Physical devices and queue families
+	VkPhysicalDevice physical_device = VK_NULL_HANDLE;
+{
+	uint32_t device_count = 0;
+	vkEnumeratePhysicalDevices(instance, &device_count, nullptr);
+	vector<VkPhysicalDevice> devices(device_count);
+	vkEnumeratePhysicalDevices(instance, &device_count, devices.data());
+
+	int32_t max_score = 0;
+	for (const auto &device : devices) {
+		VkPhysicalDeviceProperties device_properties;
+		vkGetPhysicalDeviceProperties(device, &device_properties);
+		VkPhysicalDeviceFeatures device_features;
+		vkGetPhysicalDeviceFeatures(device, &device_features);
+
+		int32_t score = 0;
+
+		if (VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU == device_properties.deviceType) { score += 1000; }
+		score += device_properties.limits.maxImageDimension2D;
+		if (!device_features.geometryShader) { score = 0; }
+
+		if (score > max_score) {
+			max_score = score;
+			cout << device << "\n";
+			physical_device = device;
+		}
+	}
+}
+	if (VK_NULL_HANDLE == physical_device) {
+		std::runtime_error("Failed to find GPUs with Vulkan support!\n");
+	} else {
+		cout << "Found Vulkan supported GPU!\n";
+	}
+	//
+	struct QueueFamilyIndices {
+		uint32_t graphicsFamily;
+	};
+
 
 	uint32_t extCout = 0;
 	vkEnumerateInstanceExtensionProperties(nullptr, &extCout, nullptr);
