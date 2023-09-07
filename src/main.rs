ccf34a2af37ee4bfe69c258328951aadeb7c6938
baiango <105705580+baiango@users.noreply.github.com>
2023-09-07 21:43:45 +0800

    FlxVec checkpoint 3: Resize
    It's really hard to get it to work.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,12 @@
-extern crate glfw;
 mod glmlib;
 mod alloc;
+mod gui_loop;
 
-use std::process::exit;
-
-use glfw::{Action, Context, Key};
 use glmlib::Vec3f;
 
 
 fn main() {
-	//----- fust_gui::glmlib::cross -----//
+	// ----- fust_gui::glmlib::cross ----- //
 	// Initialize Vec3f instances
 	let mut result = Vec3f { x: 0.0, y: 0.0, z: 0.0 };
 	let a = Vec3f { x: 1.0, y: 2.0, z: 3.0 };
@@ -23,7 +20,7 @@ fn main() {
 	// Print the result
 	println!("Result: x={}, y={}, z={}", result.x, result.y, result.z);
 
-	//----- fust_gui::glmlib::Mat4 -----//
+	// ----- fust_gui::glmlib::Mat4 ----- //
 	let mut mymat = glmlib::Mat4{ data: [0.0; 16] };
 	mymat.data[0] = 100.0;
 
@@ -31,7 +28,7 @@ fn main() {
 	glmlib::prt_mat4(&mymat);
 	}
 
-	//----- fust_gui::alloc::CVec -----//
+	// ----- fust_gui::alloc::CVec ----- //
 	let mut intvec = alloc::CVec::<i32>::new(10).unwrap();
 	intvec[9] = 100;
 	println!("{:?}", intvec);
@@ -40,38 +37,20 @@ fn main() {
 	strvec[5] = "123";
 	println!("{:?}", strvec);
 
-	//----- fust_gui::alloc::FlxVec -----//
+	// ----- fust_gui::alloc::FlxVec ----- //
 	let mut intvec = alloc::FlxVec::<u64>::new(10000).unwrap();
 	intvec[8193] = 123;
+	intvec[9999] = 9999;
 	println!("{:?}", intvec);
 	println!("{:?}", intvec[8193]);
+	println!("{:?}", intvec[9999]);
+
+	intvec.resize(9);
+	// println!("{:?}", intvec);
 
 	// Compiling error
 	//let mut strvec = alloc::FlxVec::<&str>::new(10).unwrap();
 
-	exit(0);
-	//----- glfw -----//
-	let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
-
-	let (mut window, events) = glfw.create_window(300, 300, "Hello this is window", glfw::WindowMode::Windowed)
-		.expect("Failed to create GLFW window.");
-	window.make_current();
-	window.set_key_polling(true);
-
-	while !window.should_close() {
-		// Input
-		glfw.poll_events();
-		for (_, event) in glfw::flush_messages(&events) {
-			println!("{:?}", event);
-			match event {
-				glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
-					window.set_should_close(true)
-				},
-				_ => {},
-			}
-
-		// End
-		window.swap_buffers();
-		}
-	}
+	// std::process::exit(0);
+	// gui_loop::app();
 }
